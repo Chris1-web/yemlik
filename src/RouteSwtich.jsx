6f308d41c3edb8806123fde12a08f9e58548c437
Christianah Adeyemo <christianah123.ac@gmail.com>
2024-09-13 11:44:55 -0700

    another

--- src/RouteSwtich.jsx
+++ src/RouteSwtich.jsx
@@ -4,8 +4,9 @@ import Login from "./Pages/Login/Login";
 
 function RouteSwitch() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="">
       <Routes>
+        <Route path="/" element={<p>Welcome Home</p>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
