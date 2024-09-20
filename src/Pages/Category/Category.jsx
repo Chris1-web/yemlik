bb6fa024c33aa98f158a2658dbbb0b0acbedbf8d
Christianah Adeyemo <christianah123.ac@gmail.com>
2024-09-20 10:17:17 -0700

    get category products

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -2,36 +2,63 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 
 import { db } from "../../Firebase/Firebase-init";
-import { collection, query, onSnapshot } from "firebase/firestore";
+import { collection, query, onSnapshot, where } from "firebase/firestore";
 
 import "./Category.css";
 
 function Category() {
   const { productName } = useParams();
+  // const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
 
   useState(() => {
-    async function getCategories() {
-      try {
-        const categories = query(collection(db, "categories"));
-        onSnapshot(categories, (documents) => {
-          const postDocs = [];
-          documents.forEach((doc) => {
-            postDocs.push(doc.data());
-            // setPost(postDocs[0]);
-            console.log(postDocs[0]);
-          });
+    async function getProfilePosts() {
+      const categoryProduct = query(
+        collection(db, "categories"),
+        where("type", "==", productName)
+      );
+      onSnapshot(categoryProduct, (querySnapshot) => {
+        const postDocs = [];
+        querySnapshot.forEach((doc) => {
+          postDocs.push(doc.data());
+          setProducts(postDocs);
         });
-      } catch (error) {
-        console.log(error.message);
-      }
+      });
     }
-    getCategories();
+    getProfilePosts();
   }, []);
 
   return (
-    <div>
-      <p>Welcome to the {productName} category page</p>
+    <div className="container">
+      <h1 className="uppercase text-center mt-10 text-5xl">{productName}</h1>
+      <p>
+        <a href="/">GO BACK</a>
+      </p>
       <h1 className="text-3xl">LOADING...</h1>
+      <table className="w-full border-separate border border-slate-500">
+        <thead className="text-left text-5xl bold">
+          <tr>
+            <th className="p-5">No</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody className="text-3xl gap-10">
+          {products.map((product, index) => {
+            return (
+              <tr key={product.id}>
+                <td className="border border-slate-700 p-5">{index + 1}</td>
+                <td className="border border-slate-700">{product.quantity}</td>
+                <td className="border border-slate-700">{product.price}</td>
+                <td className="border border-slate-700">
+                  <button className="w-full text-center">Add</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {console.log(products)}
     </div>
   );
 }
